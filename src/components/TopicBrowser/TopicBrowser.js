17f5071a402211969a948d8f19b108ed7d2d3cac
Madelyn Arsenault <madelynarsenault@Madelyns-Air-2.7thFlr-Rm713>
2019-08-19 21:54:08 -0500

    finalsssss

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+
+//The Topics!
+
+import EvenAndOdd from "../Topics/EvenAndOdd"
+import FilterObject from "../Topics/FilterObject"
+import FilterString from "../Topics/FilterString"
+import Palindrome from "../Topics/Palindrome"
+import Sum from "../Topics/Sum"
+
+
+
+export default class TopicBrowser extends Component {
+    render() {
+        return (
+            <div>
+                <EvenAndOdd/>
+                <FilterObject/>
+                <FilterString/>
+                <Palindrome/>
+                <Sum/>
+            </div>
+
+        )
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+export default class FilterObject extends Component {
+    constructor(){
+        super()
+        this.state ={
+            employees: [
+                {
+                    name:"Person one",
+                    title: "noob",
+                    age: 400,
+                },
+                {
+                    name: "person two",
+                    age: 3393,
+                    hairColor: "blue i guess"
+                },
+                {
+                    name: "mads",
+                    title: "game lord"
+                }
+            ],
+            userInput: "",
+            filteredEmployees: []
+        }
+    }
+    handleChange(val) {
+        this.setState({ userInput: val });
+      }
+      filterEmployees(prop) {
+
+    }
+    render() {
+        return (
+          <div className="puzzleBox filterObjectPB">
+            <h4> Filter Object </h4>
+            <span className="puzzleText"> Original: { JSON.stringify(this.state.employees, null, 10) } </span>
+            <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+            <button className="confirmationButton" onClick={ () => this.filterEmployees(this.state.userInput) }> Filter </button>
+            <span className="resultsBox filterObjectRB"> Filtered: { JSON.stringify(this.state.filteredEmployees, null, 10) } </span>
+          </div>
+        )
+      }
+}
